b05f620e6d26bd7c2376d296565205dcbfeee577
Lou Vincent Baroro <loubaroro@isufst.edu.ph>
2025-11-26 02:11:45 +0800

    Install full composer deps in Vercel build and run package discovery

--- scripts/install-composer.sh
+++ scripts/install-composer.sh
@@ -2,5 +2,7 @@
 # Download and install composer, then run composer install
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php --install-dir=/tmp --filename=composer.phar
-php /tmp/composer.phar install --no-dev --optimize-autoloader --no-interaction
+php /tmp/composer.phar install --optimize-autoloader --no-interaction
+php /tmp/composer.phar dump-autoload -o
+php artisan package:discover --ansi || true
 rm -f composer-setup.php /tmp/composer.phar
