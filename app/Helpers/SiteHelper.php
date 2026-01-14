ede04b08acc12b851a6f92f6710146b3e11e0f87
Lou Vincent Baroro <loubaroro@isufst.edu.ph>
2026-01-14 22:53:36 +0800

    Remove site name field from admin settings, use environment variable only

--- app/Helpers/SiteHelper.php
+++ app/Helpers/SiteHelper.php
@@ -2,16 +2,13 @@
 
 namespace App\Helpers;
 
-use App\Models\Setting;
-
 class SiteHelper
 {
     /**
-     * Get the site name from database or app configuration
+     * Get the site name from app configuration
      */
     public static function getSiteName()
     {
-        $setting = Setting::where('key', 'site_name')->first();
-        return $setting ? $setting->value : config('app.name', 'CICT Dingle');
+        return config('app.name', 'CICT Dingle');
     }
 }
