f22e886213ce7f399a53ab06a3e466b2b0db5816
Lou Vincent Baroro <loubaroro@isufst.edu.ph>
2026-01-17 22:39:44 +0800

    Re-enable SecurityHeaders + add Laravel storage proxy to fix Supabase CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            // \App\Http\Middleware\SecurityHeaders::class, // Disabled - blocking Supabase CORS
+            \App\Http\Middleware\SecurityHeaders::class,
         ],
 
         'api' => [
